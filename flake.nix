36bad0db4233782322eaa32cfb7f4a346ca28038
sakuya <chichengle@outlook.com>
2025-12-04 19:36:42 +0800

    feat: 引入nixos-generators以生成Proxmox镜像，并移除hardware-configuration.nix文件及其引用。

--- flake.nix
+++ flake.nix
@@ -3,15 +3,29 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixos-generators = {
+      url = "github:nix-community/nixos-generators";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { self, nixpkgs, ... }@inputs: {
+  outputs = { self, nixpkgs, nixos-generators, ... }@inputs: {
     nixosConfigurations.nixos-server = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
       modules = [
         ./configuration.nix
         ./hardware-configuration.nix
       ];
+    };
+
+    packages.x86_64-linux = {
+      proxmox = nixos-generators.nixosGenerate {
+        system = "x86_64-linux";
+        modules = [
+          ./configuration.nix
+        ];
+        format = "proxmox";
+      };
     };
   };
 }
